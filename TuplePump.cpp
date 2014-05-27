b4d5f2452a5deba1f3992749fa827ded11ba2f0b
endri2 <kekekej@gmail.com>
2014-05-27 14:03:22 +0200

    tworzenie FIFO

--- TuplePump.cpp
+++ TuplePump.cpp
@@ -1,1 +1,81 @@
 #include "TuplePump.h"
+#include "TupleMatcher.h"
+#include <sys/errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <cstdio>
+
+#define FIFO_READ 0
+#define FIFO_WRITE 1
+#define PERMS 0666
+
+std::string TuplePump::tupleFIFO = "/tmp/tupleFIFO.";
+std::string TuplePump::infoFIFO = "/tmp/infoFIFO.";
+
+TuplePump::TuplePump(const char *name, TupleMatcher *matcher) :
+		name(name), matcher(matcher), running(false), correctlyInitialied(true), fifo {
+				-1, -1 }, tupleFD { -1, -1 }, infoFD { -1, -1 } {
+
+	//int mknod(const char *pathname, mode_t mode, dev_t dev);
+
+	if (mknod((tupleFIFO + std::to_string(getpid())).c_str(), S_IFIFO | PERMS,
+			0) < 0) {
+		std::fprintf(stderr, "nie mozna utworzyc FIFO wewnetrznego: %s",
+				(tupleFIFO + std::to_string(getpid())).c_str());
+		correctlyInitialied = false;
+	}
+
+	if (mknod((infoFIFO + std::to_string(getpid())).c_str(), S_IFIFO | PERMS, 0)
+			< 0) {
+		std::fprintf(stderr, "nie mozna utworzyc FIFO wewnetrznego: %s",
+				(infoFIFO + std::to_string(getpid())).c_str());
+		correctlyInitialied = false;
+	}
+
+	if ((mknod(this->name.c_str(), S_IFIFO | PERMS, 0) < 0) && (errno != EEXIST)) {
+		std::fprintf(stderr, "nie mozna utworzyc FIFO miedzyprocesowego: %s",
+				this->name.c_str());
+		correctlyInitialied = false;
+	}
+
+}
+
+TuplePump::~TuplePump(void) {
+	stop();
+
+}
+
+int TuplePump::start(void) {
+	//otworz potoki, stworz nowy watek,w masce ustaw brak przechwytywania jakichkolwiek sygnalow
+	return -1;
+}
+
+void TuplePump::stop(void) {
+	close(fifo[0]);
+	close(fifo[1]);
+	close(tupleFD[0]);
+	close(tupleFD[1]);
+	close(infoFD[0]);
+	close(infoFD[1]);
+
+	if (unlink( (tupleFIFO + std::to_string(getpid())).c_str()) < 0) {
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s",
+				(tupleFIFO + std::to_string(getpid())).c_str());
+	}
+
+	if (unlink((infoFIFO + std::to_string(getpid())).c_str()) < 0) {
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s",
+				(infoFIFO + std::to_string(getpid())).c_str());
+	}
+
+	if (unlink(name.c_str()) < 0) {
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO miedzyprocesowego: %s",
+				name.c_str());
+	}
+}
+
+void TuplePump::putTuple(const Tuple *t) {
+
+}
--- TuplePump.h
+++ TuplePump.h
@@ -1,6 +1,8 @@
 #ifndef TUPLE_PUMP_H
 #define TUPLE_PUMP_H
 
+#include <string>
+
 class Tuple;
 class TupleSystem;
 class TupleMatcher;
@@ -12,21 +14,24 @@ class TuplePump {
 private:
 	friend class TupleSystem;
 
-	const char *name; //nazwa FIFO
+	static std::string tupleFIFO;
+	static std::string infoFIFO;
+
+	std::string name; //nazwa FIFO
 	TupleMatcher *matcher;
 	bool running; //flaga czy pompa ma pompowac czy sie odlaczyc
+	bool correctlyInitialied; //czy poprawnie udalo sie zainicjalizowac pompe (wszystkie deskryptory etc)
 
-	int FIFO[2]; //deskryptory FIFO do komunikacji miedzyprocesowej
+	int fifo[2]; //deskryptory FIFO do komunikacji miedzyprocesowej
 	int tupleFD[2]; //deskryptory FIFO/pipe sluzacy do odbierania krotek od API (wewnetrzny, wysylajcy fd jest w putTuple, a odbierajacy wewnatrz start)
-	int infoFD[2];  //deskryptory FIFO/pipe sluzacy do odbioru ktorek o informacji o zakonczeniu itp (wewnetrzne wysylajcy fd jest w stop, a odbierajacy wewnatrz start)
+	int infoFD[2]; //deskryptory FIFO/pipe sluzacy do odbioru ktorek o informacji o zakonczeniu itp (wewnetrzne wysylajcy fd jest w stop, a odbierajacy wewnatrz start)
 
 	/**
 	 * Tylko TupleSystem moze utworzyc pompe i moze ja zniszczyc
 	 * name - nazwa dla potoku nazwanego
 	 * matcher - przypisany matcher, do ktorego beda wysylane pobrane krotki z potoku
 	 */
-	TuplePump(const char *name, TupleMatcher *matcher, int tupleFD);
-
+	TuplePump(const char *name, TupleMatcher *matcher);
 
 	~TuplePump(void);
 
@@ -46,11 +51,10 @@ private:
 	 */
 	void stop(void);
 
-
 	/**
 	 * Umieszcza krotke w przestrzeni.
 	 */
-	void putTuple(Tuple t);
+	void putTuple(const Tuple *t);
 };
 
 #endif
