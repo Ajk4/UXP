0c02b30dcb406410dbc5d6d1674f2a60383f6478
Radek <radek.wiech@gmail.com>
2014-05-29 00:02:20 +0200

    Poczatkowy commit matchera

--- TupleMatcher.cpp
+++ TupleMatcher.cpp
@@ -1,1 +1,12 @@
 #include "TupleMatcher.h"
+
+TupleMatcher::TupleMatcher(int tupleSendFD)
+{
+	this->tupleSendFD = tupleSendFD;
+}
+
+int TupleMatcher::match(unsigned char *binaryTuple)
+{
+
+return 1;
+}
