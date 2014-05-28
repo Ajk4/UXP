ceb296c00b8e54303267450198b8099016d726b3
endri2 <kekekej@gmail.com>
2014-05-28 14:05:58 +0200

    potoki wewnetrzene pompy dzialaja

--- TuplePump.cpp
+++ TuplePump.cpp
@@ -1,12 +1,15 @@
+#include "Tuple.h"
 #include "TuplePump.h"
 #include "TupleMatcher.h"
 #include <sys/errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string>
 #include <cstdio>
+#include <cstring>
 
 #define FIFO_READ 0
 #define FIFO_WRITE 1
@@ -16,10 +19,8 @@ std::string TuplePump::tupleFIFO = "/tmp/tupleFIFO.";
 std::string TuplePump::infoFIFO = "/tmp/infoFIFO.";
 
 TuplePump::TuplePump(const char *name, TupleMatcher *matcher) :
-		name(name), matcher(matcher), running(false), correctlyInitialized(true), fifo {
-				-1, -1 }, tupleFD { -1, -1 }, infoFD { -1, -1 } {
-
-	//int mknod(const char *pathname, mode_t mode, dev_t dev);
+		name(name), matcher(matcher), running(false), correctlyInitialized(
+				true), fifoFD { -1, -1 }, tupleFD { -1, -1 }, infoFD { -1, -1 } {
 
 	tupleName = tupleFIFO + std::to_string(getpid());
 	infoName = infoFIFO + std::to_string(getpid());
@@ -85,51 +86,159 @@ int TuplePump::start(void) {
 		start = false;
 	}
 
-	if ((fifo[FIFO_WRITE] = open(name.c_str(), O_RDWR) < 0)) {
+	if ((fifoFD[FIFO_WRITE] = open(name.c_str(), O_RDWR) < 0)) {
 		std::fprintf(stderr, "nie mozna otworzyc FIFO wewnetrznego: %s",
 				infoName.c_str());
 		start = false;
 	}
 
-	if ((fifo[FIFO_READ] = open(name.c_str(), O_RDWR) < 0)) {
+	if ((fifoFD[FIFO_READ] = open(name.c_str(), O_RDWR) < 0)) {
 		std::fprintf(stderr, "nie mozna otworzyc FIFO wewnetrznego: %s",
 				name.c_str());
 		start = false;
 	}
 
-
-	if(start) {
-
-		std::printf("wszystko ok\n");
+	running = true;
+	if ((pthread_create(&thread, nullptr, TuplePump::_run, (void*) this))
+			!= 0) {
+		std::fprintf(stderr, "Nie mozna utworzyc nowego watku\n");
+		start = false;
+		running = false;
 	}
 
-	return -1;
+	if (!start) {
+		std::fprintf(stderr, "Wystapily bledy, nie mozna wystartowac\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+void *TuplePump::_run(void *ptr) {
+
+	auto pump = (TuplePump*) ptr;
+	std::fprintf(stderr, "new thread\n");
+
+	//znajdz deskryptor o najwieszej wartosci
+	int maxFD =
+			pump->fifoFD[FIFO_READ] > pump->infoFD[FIFO_READ] ?
+					(pump->fifoFD[FIFO_READ] > pump->tupleFD[FIFO_READ] ?
+							pump->fifoFD[FIFO_READ] : pump->tupleFD[FIFO_READ]) :
+					(pump->infoFD[FIFO_READ] > pump->tupleFD[FIFO_READ] ?
+							pump->infoFD[FIFO_READ] : pump->tupleFD[FIFO_READ]);
+
+
+	std::fprintf(stderr, "FDs: %d, %d, %d, maxFD = %d\n", pump->fifoFD[FIFO_READ], pump->infoFD[FIFO_READ], pump->tupleFD[FIFO_READ], maxFD);
+	int sel;
+	unsigned char buf[BINARY_TUPLE_LENGTH];
+
+	while (pump->running) {
+		fd_set readSet;
+		FD_ZERO(&readSet);
+
+		FD_SET(pump->fifoFD[FIFO_READ], &readSet);
+		FD_SET(pump->infoFD[FIFO_READ], &readSet);
+		FD_SET(pump->tupleFD[FIFO_READ], &readSet);
+
+		struct timeval tv;
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+
+		//zaczekaj na dostepnosc deskryptora
+		sel = select(maxFD + 1, &readSet, NULL, NULL, &tv);
+
+		if (sel < 0) {
+			std::fprintf(stderr, "select error\n");
+			return ptr;
+		} else if (sel == 0) {
+			std::fprintf(stderr, "timeout\n");
+		} else {
+			if (FD_ISSET(pump->tupleFD[FIFO_READ], &readSet)) {
+				//odbierz krotke do wyslania w przestrzen
+
+
+				read(pump->tupleFD[FIFO_READ], buf, BINARY_TUPLE_LENGTH);
+
+				std::fprintf(stderr, "odebrano krotke do wysylki: %c\n", buf[4]);
+
+				int i = write(pump->fifoFD[FIFO_WRITE], buf, BINARY_TUPLE_LENGTH);
+				std::fprintf(stderr, "wysylanie, kod wyslania: %d/%d\n", i, BINARY_TUPLE_LENGTH);
+				//write(pump->fifo[FIFO_WRITE], buf, BINARY_TUPLE_LENGTH);
+			} else if (FD_ISSET(pump->infoFD[FIFO_READ], &readSet)) {
+				//dotarla jakas informacja (np o zakocnzeniu)
+				//informacje obsluguje sie kiedy nie ma nic do wysylki
+				//dzieki czemu unika sie sytuacji kiedy po kilku wrzuceniach krotek
+				//puszcza sie informacje o stopie wszystkie krotki zostana wyslane
+				//zanim ten stop zajdzie
+				int data[1];
+				read(pump->infoFD[FIFO_READ], data, 1);
+				//na te chwile informacja jest tylko o zakonczeniu
+				std::fprintf(stderr, "konczenie\n");
+				return ptr;
+			}
+
+			if (FD_ISSET(pump->fifoFD[FIFO_READ], &readSet)) {
+				//standardowe przepompowywanie krotek
+				std::fprintf(stderr, "czytanie\n");
+				read(pump->fifoFD[FIFO_READ], buf, BINARY_TUPLE_LENGTH);
+
+				std::fprintf(stderr, "odebrano krotke z przestrzeni: %c\n", buf[4]);
+
+				/*int result = pump->matcher->match(buf);
+				if(result == -1 || result == 1) {
+				//zmniejszyc TTL i wywalic krotke do przestrzeni z powrotem
+				write(pump->fifoFD[FIFO_WRITE], buf, BINARY_TUPLE_LENGTH);
+				}
+				*/
+
+			}
+
+		}
+	}
+
+	return ptr;
 }
 
 void TuplePump::stop(void) {
-	close(fifo[0]);
-	close(fifo[1]);
+
+	//wyslij informacje o zakonczeniu
+	int info[] = { 1 };
+	write(infoFD[FIFO_WRITE], info, sizeof(info));
+
+	//poczekaj na watek obslugujacy pompe
+	pthread_join(thread, nullptr);
+
+	//pozamykaj wszystkie deskryptory
+	close(fifoFD[0]);
+	close(fifoFD[1]);
 	close(tupleFD[0]);
 	close(tupleFD[1]);
 	close(infoFD[0]);
 	close(infoFD[1]);
 
 	if (unlink((tupleFIFO + std::to_string(getpid())).c_str()) < 0) {
-		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s",
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s\n",
 				(tupleFIFO + std::to_string(getpid())).c_str());
 	}
 
 	if (unlink((infoFIFO + std::to_string(getpid())).c_str()) < 0) {
-		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s",
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO wewnetrznego: %s\n",
 				(infoFIFO + std::to_string(getpid())).c_str());
 	}
 
 	if (unlink(name.c_str()) < 0) {
-		std::fprintf(stderr, "nie mozna odlaczyc FIFO miedzyprocesowego: %s",
+		std::fprintf(stderr, "nie mozna odlaczyc FIFO miedzyprocesowego: %s\n",
 				name.c_str());
 	}
 }
 
 void TuplePump::putTuple(const Tuple *t) {
+	static unsigned char c = (unsigned char)0;//[PH]dane "krotki"
+	unsigned char buf[BINARY_TUPLE_LENGTH];
+	std::memset(buf, 0, BINARY_TUPLE_LENGTH);//[PH]
+	//t->getBinaryRepresentation(buf, DEFAULT_TTL); //NYI
+	buf[4] = ((++c) % 90) + 33;//[PH] dane binarne krotki
+
+	write(tupleFD[FIFO_WRITE], buf, BINARY_TUPLE_LENGTH);
 
 }
--- TuplePump.h
+++ TuplePump.h
@@ -2,6 +2,7 @@
 #define TUPLE_PUMP_H
 
 #include <string>
+#include <pthread.h>
 
 class Tuple;
 class TupleSystem;
@@ -18,13 +19,15 @@ private:
 	static std::string infoFIFO;
 
 	std::string tupleName; //nazwa wewntrznego FIFO do synchronizacji odbierania krotek
-	std::string infoName; //nazwa wewntrznego FIFO do synchronizacji odbierania informacji
+	std::string infoName; //nazwa wewntrznego FIFO do synchronizacji odbierania informacji o zakonczeniu
 	std::string name; //nazwa FIFO do komunikacji miedzyprocesowej
+
 	TupleMatcher *matcher;
 	bool running; //flaga czy pompa ma pompowac czy sie odlaczyc
 	bool correctlyInitialized; //czy poprawnie udalo sie zainicjalizowac pompe (wszystkie deskryptory etc)
+	pthread_t thread;
 
-	int fifo[2]; //deskryptory FIFO do komunikacji miedzyprocesowej
+	int fifoFD[2]; //deskryptory FIFO do komunikacji miedzyprocesowej
 	int tupleFD[2]; //deskryptory FIFO/pipe sluzacy do odbierania krotek od API (wewnetrzny, wysylajcy fd jest w putTuple, a odbierajacy wewnatrz start)
 	int infoFD[2]; //deskryptory FIFO/pipe sluzacy do odbioru ktorek o informacji o zakonczeniu itp (wewnetrzne wysylajcy fd jest w stop, a odbierajacy wewnatrz start)
 
@@ -38,15 +41,23 @@ private:
 	~TuplePump(void);
 
 	/**
-	 * Dolacza do potoku nazwanego i wykonuje obsluge zwiazana z pobieraniem krotek,
-	 * Wysylaniem ich do matchera i odsylaniem z powrotem do przestrzeni.
-	 * Wysyla takze nowe krotki w przestrzen.
+	 * Uruchamia pompe poprzez otworzenie potokow wewnetrzenych
+	 * i dolaczeniu do potoku nazwanego do komunikacji miedzyprocesowej.
 	 *
-	 * Metoda dzialajaca w petli, dopoki nie zostanie wywolana metoda stop.
+	 * Uruchamia obsluge krotek, ktora bedzie sie dziac w oddzielnym watku.
 	 *
 	 * Zwraca -1 jezeli nie udalo sie dolaczyc do przestrzeni
 	 */
 	int start(void);
+
+	/*
+	 * wykonuje obsluge zwiazana z pobieraniem krotek,
+	 * Wysylaniem ich do matchera i odsylaniem z powrotem do przestrzeni.
+	 * Wysyla takze nowe krotki w przestrzen.
+	 *
+	 * Metoda dzialajaca w petli, dopoki nie zostanie wywolana metoda stop.
+	 */
+	static void *_run(void *ptr);
 
 	/**
 	 * Zakancza dzialanie pompy i odlacza ja od potoku nazwanego
