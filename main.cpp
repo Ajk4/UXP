121a2bc15bc549a11d2df255dda5b7fa82100822
Radek <radek.wiech@gmail.com>
2014-05-30 02:33:52 +0200

    Dodano Porownywanie intow i floatow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "TupleMatcher.h"
 #include "TuplePattern.h"
+union {
+	int integer;
+	float floatVariable;
+} conversionUnion;
 
 int main(int argc, char **argv) {
 
@@ -8,11 +12,20 @@ int main(int argc, char **argv) {
 		std::cout << argv[i] << std::endl;
 	}
 
-	unsigned char testchar[] = "radek";
+	unsigned char tab[4]  = { 0x0,0x0,0x0, 0xFA};
+	conversionUnion.integer  = 0xFA;
+	float x =conversionUnion.floatVariable;
+	std::cout <<"wysłano x " << x << std::endl;
+	TupleMatcher *match;
+	match = new TupleMatcher(1);
+	match->CheckFloat(tab, 1.23, 0);
+
+
+	/*unsigned char testchar[] = {0x00, 0x00, 0x01, 0x04};
 	
 	TupleMatcher *match;
 	match = new TupleMatcher(1);
-	if(match-> CheckString(testchar, "Jeden", TuplePattern::GT))
-		std::cout<< "prawda"<<std::endl;
+	if(match-> CheckInteger(testchar, 4, TuplePattern::GT))
+		std::cout<< "prawda"<<std::endl;*/
 	return 0;
 }
