bd86b6fc2553cdd99d83b625a78d0d2f8c1a2ef0
ajk <jakubowski.adam@gmail.com>
2014-06-05 18:26:55 +0200

    Proste programy wysylajace/odbierajace tuple

---
+++ sender.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+#include "Tuple.h"
+#include "TupleSystem.h"
+
+#include <unistd.h>
+
+int main(int _argc, char **_argv) {
+    char *fifoName = _argv[1];
+
+    TupleSystem tupleSystem;
+    tupleSystem.lindaOpen(fifoName);
+
+    Tuple tuple;
+    tuple.append(2.0f);
+    tuple.append("asd");
+    tuple.append(3);
+    while (true){
+        std::cout << "Sending tuple" << std::cout;
+        tupleSystem.lindaOutput(&tuple);
+        usleep(5000);
+    }
+
+    tupleSystem.lindaClose();
+
+}
