3ae2ac3e44f46f5ffe563ac909ee90638fe399cb
ajk <jakubowski.adam@gmail.com>
2014-06-06 11:41:56 +0200

    Main2 -> test na wiecej procesow

--- main2.cpp
+++ main2.cpp
@@ -18,7 +18,49 @@ int main(int argc, char **argv) {
 
         sys.lindaOutput(&t);
         sys.lindaClose();
-    } else {
+    }
+    if(fork()){
+        TupleSystem sys;
+        sys.lindaOpen("main2");
+
+        Tuple t;
+        t.append(2.0f);
+        t.append("ke");
+        t.append(33);
+
+        sys.lindaOutput(&t);
+        sys.lindaClose();
+    }
+    if(fork()){
+        TupleSystem sys;
+        sys.lindaOpen("main2");
+
+        Tuple t;
+        t.append("wat");
+        t.append("ke");
+        t.append(33);
+
+        sys.lindaOutput(&t);
+        sys.lindaClose();
+    }
+
+
+    {
+        TupleSystem sys;
+        sys.lindaOpen("main2");
+
+        TuplePattern p;
+        p.appendString(TuplePattern::ANY);
+        p.appendString(TuplePattern::ANY);
+        p.appendInt(TuplePattern::ANY);
+
+        Tuple *output = NULL;
+
+        sys.lindaRead(&p, 5, &output);
+        sys.lindaClose();
+    }
+
+    {
         TupleSystem sys;
         sys.lindaOpen("main2");
 
